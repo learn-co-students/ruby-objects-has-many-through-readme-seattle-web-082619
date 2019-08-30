20e0da3b19a05528bbeb7197b738e495faa974f5
ianhunterharold <ianhunterharold@gmail.com>
2019-08-30 14:34:44 -0700

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,22 @@
 class Meal
 
+    attr_reader :waiter, :customer, :total, :tip
+
+    @@all = []
+    #array to store all of our instances  
+
+    def initialize(waiter,customer,total,tip)
+        @waiter  = waiter
+        @customer = customer
+        @total = total
+        @tip = tip
+        @@all << self 
+        #self in this context is the instance that is created 
+    end 
+
+    def self.all 
+        @@all 
+    end 
+
+
 end
